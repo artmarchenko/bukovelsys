047eb79ea617ffcf65277e2e0a52119418cdf7f4
Artem Marchenko <art.marchenko@gmail.com>
2019-04-18 02:50:15 +0300

    New Main

--- src/main/java/bokovelsys/Main.java
+++ src/main/java/bokovelsys/Main.java
@@ -5,6 +5,8 @@ import bokovelsys.cards.SkiPassType;
 import bokovelsys.servises.AccountSystem;
 import bokovelsys.servises.Turnstile;
 
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -19,7 +21,18 @@ public class Main {
 //        System.out.println(daySkiPass.getId());
 //        System.out.println(daySkiPass2.getId());
         Turnstile turnstile = new Turnstile();
-        turnstile.getStat(SkiPassType.SEASON);
+        Map<SkiPassType,Integer> mp = turnstile.getCounterPassed();
+        mp.put(SkiPassType.SEASON, 5);
+        mp.put(SkiPassType.HALF_DAY_AFTERNOON, 2);
+
+        Map<SkiPassType,Integer> mc = turnstile.getCounterCanceled();
+        mc.put(SkiPassType.DAY_LIMIT, 3);
+        mc.put(SkiPassType.SEASON, 1);
+
+
+        turnstile.getStat();
+
+        turnstile.getStat(SkiPassType.DAY_LIMIT);
 
     }
 
