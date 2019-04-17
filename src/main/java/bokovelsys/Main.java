146bc52c8a8abdc0985452e1876de6eea018a8ee
Artem <art.marchenko@gmail.com>
2019-04-17 22:17:21 +0300

    new Main

--- src/main/java/bokovelsys/Main.java
+++ src/main/java/bokovelsys/Main.java
@@ -1,7 +1,9 @@
 package bokovelsys;
 
 import bokovelsys.cards.SkiPass;
+import bokovelsys.cards.SkiPassType;
 import bokovelsys.servises.AccountSystem;
+import bokovelsys.servises.Turnstile;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,9 +13,13 @@ public class Main {
         SkiPass daySkiPass = accountSystem.createDaySkiPass(5);
         SkiPass daySkiPass2 = accountSystem.createDaySkiPass(5);
 
-
-        System.out.println(daySkiPass.getId());
-        System.out.println(daySkiPass2.getId());
+//        System.out.println(accountSystem.getSkiPassList().size());
+//
+//
+//        System.out.println(daySkiPass.getId());
+//        System.out.println(daySkiPass2.getId());
+        Turnstile turnstile = new Turnstile();
+        turnstile.getStat(SkiPassType.SEASON);
 
     }
 
