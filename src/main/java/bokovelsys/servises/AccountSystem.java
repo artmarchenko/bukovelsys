f7758d949317295c550192e07ddc28f907040625
Artem <art.marchenko@gmail.com>
2019-04-24 22:23:33 +0300

    add Validator

--- src/main/java/bokovelsys/servises/AccountSystem.java
+++ src/main/java/bokovelsys/servises/AccountSystem.java
@@ -2,78 +2,73 @@ package bokovelsys.servises;
 
 import bokovelsys.cards.*;
 
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
 
 public class AccountSystem {
 
-        private List<SkiPass> skiPassList = new LinkedList<>();
-        private int lastSkiPassId;
-        private int startMonthOfSeason = 9;
-        private int endMonthOfSeason = 4;
+    private List<SkiPass> skiPassList = new LinkedList<>();
+    private int lastSkiPassId;
+    private int startMonthOfSeason = 9;
+    private int endMonthOfSeason = 4;
 
+    private static class AccountSystemHolder {
+        public static final AccountSystem ACCOUNT_SYSTEM_INSTANCE = new AccountSystem();
+    }
 
+    public static AccountSystem getAccountingSystem() {
+        return AccountSystemHolder.ACCOUNT_SYSTEM_INSTANCE;
+    }
 
-        private   static class AccountSystemHolder{
-            public static final AccountSystem ACCOUNT_SYSTEM_INSTANCE = new AccountSystem();
-        }
+    public SkiPass createHalfDayMorningSkiPass() {
+        HalfDayMorningSkiPass skiPass = new HalfDayMorningSkiPass();
+        skiPassList.add(skiPass);
+        return skiPass;
+    }
 
-        public static AccountSystem getAccountingSystem(){
-            return AccountSystemHolder.ACCOUNT_SYSTEM_INSTANCE;
-        }
+    public SkiPass createHalfDayAfternoonSkiPass() {
+        HalfDayAfternoonSkiPass skiPass = new HalfDayAfternoonSkiPass();
+        skiPassList.add(skiPass);
+        return skiPass;
+    }
 
-        public SkiPass  createHalfDayMorningSkiPass(){
-            HalfDayMorningSkiPass skiPass = new HalfDayMorningSkiPass();
-            skiPassList.add(skiPass);
-            return skiPass;
+    public SkiPass createDaySkiPass(int days) {
+        DaySkiPass skiPass = new DaySkiPass(days);
+        skiPassList.add(skiPass);
+        return skiPass;
+    }
 
-        }
+    public SkiPass createHolidayHalfDayMorningSkiPass() {
+        HalfDayMorningSkiPass skiPass = new HalfDayMorningSkiPass();
+        skiPassList.add(skiPass);
+        return skiPass;
+    }
 
-        public SkiPass  createHalfDayAfternoonSkiPass(){
-            HalfDayAfternoonSkiPass skiPass = new HalfDayAfternoonSkiPass();
-            skiPassList.add(skiPass);
-            return skiPass;
-        }
+    public SkiPass createHolidayHalfDayAfternoonSkiPass() {
+        HolidayHalfDayAfternoonSkiPass skiPass = new HolidayHalfDayAfternoonSkiPass();
+        skiPassList.add(skiPass);
+        return skiPass;
+    }
 
-        public SkiPass  createDaySkiPass(int days){
-            DaySkiPass skiPass = new DaySkiPass(days);
-            skiPassList.add(skiPass);
-            return skiPass;
-        }
+    public SkiPass createHolidayDaySkiPass(int days) {
+        HolidayDaySkiPass skiPass = new HolidayDaySkiPass(days);
+        skiPassList.add(skiPass);
+        return skiPass;
+    }
 
-        public SkiPass  createHolidayHalfDayMorningSkiPass(){
-            HalfDayMorningSkiPass skiPass = new HalfDayMorningSkiPass();
-            skiPassList.add(skiPass);
-            return skiPass;
-        }
+    public SkiPass createSeasonSkiPass() {
+        SeasonSkiPass skiPass = new SeasonSkiPass();
+        skiPassList.add(skiPass);
+        return skiPass;
+    }
 
-        public SkiPass  createHolidayHalfDayAfternoonSkiPass(){
-            HolidayHalfDayAfternoonSkiPass skiPass = new HolidayHalfDayAfternoonSkiPass();
-            skiPassList.add(skiPass);
-            return skiPass;
-        }
+    public void blockSkiPass(SkiPass skiPass) {
+        skiPass.setBlocked(true);
+    }
 
-        public SkiPass createHolidayDaySkiPass(int days){
-            HolidayDaySkiPass skiPass = new HolidayDaySkiPass(days);
-            skiPassList.add(skiPass);
-            return skiPass;
-        }
-
-        public SkiPass  createSeasonSkiPass(){
-            SeasonSkiPass skiPass = new SeasonSkiPass();
-            skiPassList.add(skiPass);
-            return skiPass;
-        }
-
-        public void blockSkiPass(SkiPass skiPass){
-            skiPass.setBlocked(true);
-        }
-
-        public int getNextId(){
-            return ++this.lastSkiPassId;
-        }
+    public int getNextId() {
+        return ++this.lastSkiPassId;
+    }
 
     public int getStartMonthOfSeason() {
         return startMonthOfSeason;
--- src/main/java/bokovelsys/servises/Turnstile.java
+++ src/main/java/bokovelsys/servises/Turnstile.java
@@ -1,23 +1,24 @@
 package bokovelsys.servises;
 
+import bokovelsys.cards.SkiPass;
 import bokovelsys.cards.SkiPassType;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Turnstile {
-    private Map<SkiPassType,Integer> counterPassed = new HashMap<>();
-    private Map<SkiPassType,Integer> counterCanceled = new HashMap<>();
+    private Map<SkiPassType, Integer> counterPassed = new HashMap<>();
+    private Map<SkiPassType, Integer> counterCanceled = new HashMap<>();
+    private Validator validator = new Validator;
 
     public Turnstile() {
-        for (SkiPassType skiPassType : SkiPassType.values()){
+        for (SkiPassType skiPassType : SkiPassType.values()) {
             counterPassed.put(skiPassType, 0);
         }
 
-        for (SkiPassType skiPassType : SkiPassType.values()){
+        for (SkiPassType skiPassType : SkiPassType.values()) {
             counterCanceled.put(skiPassType, 0);
         }
-
     }
 
     public Map<SkiPassType, Integer> getCounterPassed() {
@@ -36,54 +37,56 @@ public class Turnstile {
         this.counterCanceled = counterCanceled;
     }
 
-    public void getStat(){
+    public void getStat() {
         int counterPass = 0;
         int counterCansel = 0;
 
-        for (Map.Entry<SkiPassType,Integer> entry : counterPassed.entrySet()){
+        for (Map.Entry<SkiPassType, Integer> entry : counterPassed.entrySet()) {
             counterPass = counterPass + entry.getValue();
         }
 
-        for (Map.Entry<SkiPassType,Integer> entry : counterCanceled.entrySet()){
+        for (Map.Entry<SkiPassType, Integer> entry : counterCanceled.entrySet()) {
             counterCansel = counterCansel + entry.getValue();
         }
 
-        System.out.println("Total passed: " + counterPass + ". " + "Total canceled: " + counterCansel );
+        System.out.println("Total passed: " + counterPass + ". " + "Total canceled: " + counterCansel);
     }
 
-    public void getStat(SkiPassType skiPassType){
-
-        switch (skiPassType){
+    public void getStat(SkiPassType skiPassType) {
+        switch (skiPassType) {
             case SEASON:
-                System.out.println("Total passed SEASON Skipass: " + counterPassed.get(skiPassType) + ". " + "Total canceled SEASON Skipass: " + counterCanceled.get(skiPassType) );
+                System.out.println("Total passed SEASON Skipass: " + counterPassed.get(skiPassType) + ". " + "Total canceled SEASON Skipass: " + counterCanceled.get(skiPassType));
                 break;
 
             case DAY_LIMIT:
-                System.out.println("Total passed Daylimit Skipass: " + counterPassed.get(skiPassType) + ". " + "Total canceled Daylimit Skipass: " + counterCanceled.get(skiPassType) );
+                System.out.println("Total passed Daylimit Skipass: " + counterPassed.get(skiPassType) + ". " + "Total canceled Daylimit Skipass: " + counterCanceled.get(skiPassType));
                 break;
 
             case HOLIDAY_DAY_LIMIT:
-                System.out.println("Total passed HOLIDAY_DAY_LIMIT Skipass: " + counterPassed.get(skiPassType) + ". " + "Total canceled HOLIDAY_DAY_LIMIT Skipass: " + counterCanceled.get(skiPassType) );
+                System.out.println("Total passed HOLIDAY_DAY_LIMIT Skipass: " + counterPassed.get(skiPassType) + ". " + "Total canceled HOLIDAY_DAY_LIMIT Skipass: " + counterCanceled.get(skiPassType));
                 break;
 
             case HALF_DAY_AFTERNOON:
-                System.out.println("Total passed Ha HALF_DAY_AFTERNOON Skipass: " + counterPassed.get(skiPassType) + ". " + "Total canceled HALF_DAY_AFTERNOON Skipass: " + counterCanceled.get(skiPassType) );
+                System.out.println("Total passed Ha HALF_DAY_AFTERNOON Skipass: " + counterPassed.get(skiPassType) + ". " + "Total canceled HALF_DAY_AFTERNOON Skipass: " + counterCanceled.get(skiPassType));
                 break;
 
             case HALF_DAY_MORNING:
-                System.out.println("Total passed Ha HALF_DAY_MORNING Skipass: " + counterPassed.get(skiPassType) + ". " + "Total canceled HALF_DAY_MORNING Skipass: " + counterCanceled.get(skiPassType) );
+                System.out.println("Total passed Ha HALF_DAY_MORNING Skipass: " + counterPassed.get(skiPassType) + ". " + "Total canceled HALF_DAY_MORNING Skipass: " + counterCanceled.get(skiPassType));
                 break;
 
             case HOLIDAY_HALF_DAY_MORNING:
-                System.out.println("Total passed Ha HOLIDAY_HALF_DAY_MORNING Skipass: " + counterPassed.get(skiPassType) + ". " + "Total canceled HOLIDAY_HALF_DAY_MORNING Skipass: " + counterCanceled.get(skiPassType) );
+                System.out.println("Total passed Ha HOLIDAY_HALF_DAY_MORNING Skipass: " + counterPassed.get(skiPassType) + ". " + "Total canceled HOLIDAY_HALF_DAY_MORNING Skipass: " + counterCanceled.get(skiPassType));
                 break;
 
             case HOLIDAY_HALF_DAY_AFTERNOON:
-                System.out.println("Total passed Ha HOLIDAY_HALF_DAY_AFTERNOON Skipass: " + counterPassed.get(skiPassType) + ". " + "Total canceled HOLIDAY_HALF_DAY_AFTERNOON Skipass: " + counterCanceled.get(skiPassType) );
+                System.out.println("Total passed Ha HOLIDAY_HALF_DAY_AFTERNOON Skipass: " + counterPassed.get(skiPassType) + ". " + "Total canceled HOLIDAY_HALF_DAY_AFTERNOON Skipass: " + counterCanceled.get(skiPassType));
                 break;
         }
 
 
+    }
 
+    public boolean validate(SkiPass skiPass){
+        return this.validator.validate(skiPass);
     }
 }
